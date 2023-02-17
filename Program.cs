ff9d8e98993207ce03b0ba16b0cf064a7cbfb893
sahmemmed hebibov <sahmemmedhebibov4@gmail.com>
2023-02-17 17:47:23 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,478 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             static void Main(string[] args)
+			{
+				#region task1
+				//	Console.WriteLine("programa xos geldiniz!");
+				//Label:
+				//	int y = readint();
+				//	if (y > 1000 && y < 10000)
+				//	{
+				//		y = 7 * 100000 + y * 10 + 8;
+				//		Console.WriteLine(y);
+				//	}
+				//	else
+				//	{
+				//		Console.WriteLine("eded [1000,9999] araliginda olmalidir!");
+				//		goto Label;
+				//	}
+				#endregion
+				#region task2
+				//	Console.WriteLine("programa xos geldiniz!");
+				//Label:
+				//	int y = readint();
+				//	if (y > 100 && y < 1000)
+				//	{
+				//		y = y * 1000 + y;
+				//		Console.Write(y);
+				//	}
+				//	else
+				//	{
+				//		Console.WriteLine("eded [100,999] araliginda olmalidir!");
+				//		goto Label;
+				//	}
+				#endregion
+				#region task3
+				//	Console.WriteLine("programa xos geldiniz!");
+				//Label:
+				//	int y = readint();
+				//	double per18, per3;
+				//	if (y > 10000 && y < 1000000)
+				//	{
+				//		per18 = (y / 100) * 18;
+				//		per3 = (y / 100) * 3;
+				//		Console.WriteLine($"18 % of the number={per18}");
+				//		Console.WriteLine($"3 % of the number={per3}");
+				//	}
+				//	else
+				//	{
+				//		Console.WriteLine("eded [10000,99999] araliginda olmalidir!");
+				//		goto Label;
+				//	}
+				#endregion
+				#region task4
+				//	Console.WriteLine("programa xos geldiniz!");
+				//Label:
+				//	int y = readint();
+				//	double per;
+				//	if (y > 100 && y < 1000)
+				//	{
+				//		y = y * 10 + 7;
+				//		Console.WriteLine(y);
+				//		per = (y / 100) * 7;
+				//		Console.WriteLine($"7% of {y} number={per}");
+				//	}
+				//	else
+				//	{
+				//		Console.WriteLine("eded [100,999] araliginda olmalidir!");
+				//		goto Label;
+				//	}
+				#endregion
+				#region task5
+				//	Console.WriteLine("programa xos geldiniz!");
+				//Label:
+				//	int y = readint();
+				//	double per;
+				//	if (y > 1000 && y < 10000)
+				//	{
+				//		y = 4 * 1000000 + y * 100 + 44;
+				//		Console.WriteLine(y);
+				//		per = (y / 100) * 44;
+				//				Console.WriteLine($"44% of {y} number={per}");
+				//	}
+				//	else
+				//	{
+				//		Console.WriteLine("eded [1000,9999] araliginda olmalidir!");
+				//		goto Label;
+				//	}
+				#endregion
+				#region task6
+				//	Console.WriteLine("programa xos geldiniz!");
+				//Label:
+				//	int y = readint();
+
+				//	double per;
+				//	if (y > 1000 && y < 10000)
+				//	{
+
+				//		per = (y / 100) * 20;
+				//		Console.WriteLine($"20% of {y} number={per}");
+				//		per = (per / 100) * 10;
+				//		Console.WriteLine($"10% ={per}");
+				//		Console.WriteLine($"square of {per} number={per*per}");
+				//	}
+				//	else
+				//	{
+				//		Console.WriteLine("eded [1000,9999] araliginda olmalidir!");
+				//		goto Label;
+				//	}
+				#endregion
+				#region task7
+				//Console.WriteLine("programa xos geldiniz!");
+
+				//int y = number5();
+				//int x = number5();
+				//int sum;
+				//sum = x + y;
+				//Console.WriteLine($"sum of x and y={sum}");
+				//sum = 5 * 100000 + sum * 10 + 5;
+				//Console.WriteLine($"new number={sum}");
+				//Console.WriteLine($"5% of new number={(sum/100)*5}");
+
+
+
+				#endregion
+				#region task8
+				//Console.WriteLine("programa xos geldiniz!");
+
+				//int y = number4();
+				//int x = number7();
+				//double per1,per2,sum,per3;
+				//per1 = (y / 100) * 4;
+				//Console.WriteLine($"4% of first number={per1}");
+				//         per2 = (x / 100) * 9;
+				//Console.WriteLine($"9% of second number={per2}");
+				//sum = per1 + per2;
+				//Console.WriteLine($"the sum of percentages={per2}");
+				//per3 = (sum/ 100) * 10;
+				//Console.WriteLine($"10% of sum={per3}");
+
+
+
+
+
+
+				#endregion
+				#region task9
+				//Console.WriteLine("programa xos geldiniz!");
+
+				//int y = number6();
+				//int x = number6();
+				//int z = number6();
+				//double per1, per2, sum, per3,per4;
+				//per1 = (y / 100) * 10;
+				//Console.WriteLine($"10% of first number={per1}");
+				//per2 = (x / 100) * 10;
+				//Console.WriteLine($"10% of second number={per2}");
+				//per3 = (z / 100) * 10;
+				//Console.WriteLine($"10% of third number={per3}");
+				//sum = per1 + per2+per3;
+				//Console.WriteLine($"the sum of percentages={sum}");
+				//per4 = (sum / 100) * 10;
+				//Console.WriteLine($"10% of sum={per4}");
+
+
+
+
+
+
+				#endregion
+				#region task10
+				//Console.WriteLine("programa xos geldiniz!");
+
+				//int y = number4();
+				//int x = number4();
+				//int z = number4();
+				//double per1, per2, subtraction, per3, sum;
+				//per1 = (y / 100) * 1;
+				//Console.WriteLine($"1% of first number={per1}");
+				//per2 = (x / 100) * 2;
+				//Console.WriteLine($"2% of second number={per2}");
+				//per3 = (z / 100) * 3;
+				//Console.WriteLine($"3% of third number={per3}");
+				//subtraction = per1 - per2 - per3;
+				//Console.WriteLine($"the subtraction of percentages={subtraction}");
+				//sum = subtraction + (z / 100) * 7;
+				//Console.WriteLine($"sum of subtraction and 7% of third number={sum}");
+				#endregion
+				#region task11
+				//Console.WriteLine("programa xos geldiniz!");
+
+				//int y = number5();
+				//int x = number5();
+				//int z = number5();
+				//int s = number5();
+
+				//double sum1,sum2,total,sub;
+				//sum1 = x + z;
+				//Console.WriteLine($"sum of first and third numbers={sum1}");
+				//sum2 = y + s;
+				//Console.WriteLine($"sum of second and fourth numbers={sum2}");
+				//total = sum1 * sum2;
+				//Console.WriteLine($"total of sum1 and sum2 ={total}");
+				//sub = total - (z / 100) * 3;
+				//Console.WriteLine($"subtraction of total and 3% of third number ={sub}");
+
+
+
+				#endregion
+				#region task12
+				//Console.WriteLine("programa xos geldiniz!");
+
+				//int y = number6();
+				//int x = number6();
+				//int z = number6();
+				//int s = number6();
+				//double per1, per2, sum1,sum2, per3, per4,total;
+				//per1 = (y / 100) * 10;
+				//Console.WriteLine($"10% of first number={per1}");
+				//per2 = (x / 100) * 10;
+				//Console.WriteLine($"10% of second number={per2}");
+				//per3 = (z / 100) * 10;
+				//Console.WriteLine($"10% of third number={per3}");
+				//per4 = (s/ 100) * 10;
+				//Console.WriteLine($"10% of fourth number={per4}");
+				//sum1 = per1 + per2 + per3+per4;
+				//Console.WriteLine($"the sum of percentages={sum1}");
+
+				//per1 = (y / 100) * 15;
+				//Console.WriteLine($"15% of first number={per1}");
+				//per2 = (x / 100) * 15;
+				//Console.WriteLine($"15% of second number={per2}");
+				//per3 = (z / 100) * 15;
+				//Console.WriteLine($"15% of third number={per3}");
+				//per4 = (s / 100) * 15;
+				//Console.WriteLine($"15% of fourth number={per4}");
+				//sum2 = per1 + per2 + per3 + per4;
+				//Console.WriteLine($"the sum of percentages={sum2}");
+
+				//total = sum1 * sum2;
+				//Console.WriteLine($"the total of sums={total}");
+				//per1 = (total / 100) * 10;
+				//Console.WriteLine($"10 of total={per1}");
+				//per2 = (per1 / 100) * 11;
+				//Console.WriteLine($"11 of (10% of total)={per2}");
+
+
+
+
+
+
+				#endregion
+				#region task13
+				//Console.WriteLine("programa xos geldiniz!");
+
+				//int y = number5();
+				//int x = number5();
+				//int z = number5();
+				//int s = number3();
+				//int n = number3();
+				//double per1, per2, sum, per3, per4, per5, total;
+				//per1 = (y / 100) * 5;
+				//Console.WriteLine($"5% of first number={per1}");
+				//per2 = (x / 100) * 5;
+				//Console.WriteLine($"5% of second number={per2}");
+				//per3 = (z / 100) * 5;
+				//Console.WriteLine($"5% of third number={per3}");
+				//total = per1 * per2 * per3;
+				//Console.WriteLine($"total of percentages={total}");
+				//per4 = (s / 100) * 3;
+				//Console.WriteLine($"3% of forth number={per4}");
+				//per5 = (n / 100) * 3;
+				//Console.WriteLine($"3% of fifth number={per5}");
+				//sum = per4 + per5;
+				//Console.WriteLine($"sum of percentages={sum}");
+				//sum = (total / 100) * 10 + (sum / 100) * 10;
+				//Console.WriteLine($"sum of (10% of total) and (10% of sum)={sum}");
+
+
+
+
+
+
+				#endregion
+				#region task14
+				//Console.WriteLine("programa xos geldiniz!");
+
+				//int y = number6();
+				//int x = number6();
+				//int z = number6();
+				//int s = number6();
+				//int n = number6();
+				//int m = number6();
+
+
+				//double per1, sum, sum1, stick, sub;
+				//sum = y + x + z + s + n + m;
+				//Console.WriteLine($"sum of numbers={sum}");
+				//stick = y * 1000000 + z;
+				//Console.WriteLine($"sticking of 1st and 3rd numbers={stick}");
+				//sub = sum - stick;
+				//Console.WriteLine($"subtraction of sum and stick numbers={sub}");
+				//per1 = sub / 10;
+				//Console.WriteLine($"10% of subtraction={per1}");
+				//sum1 = per1 + n + m;
+				//Console.WriteLine($"sum of 5th ,6th and (10% of sub)={sum1}");
+				//per1 = (sum1 / 100) * 11;
+				//Console.WriteLine($"11% of sum={per1}");
+
+
+
+
+
+				#endregion
+				#region task15
+				//Console.WriteLine("programa xos geldiniz!");
+
+				//int y = number3();
+				//int x = number3();
+				//int z = number4();
+				//int s = number4();
+				//int n = number5();
+				//int m = number5();
+				//int p = number6();
+
+
+				//double per1, sum, sum1, stick, sub, total;
+				//sum = y + x;
+				//Console.WriteLine($"sum of 3digits numbers={sum}");
+				//total = z * s;
+				//Console.WriteLine($"total of 4digits numbers={total}");
+				//sum = total + sum;
+				//Console.WriteLine($"sum of total and sum={sum}");
+				//sum = sum * 10 + 7;
+				//Console.WriteLine($"adding 7 at the end of sum={sum}");
+				//sum1 = n + m;
+				//Console.WriteLine($"sum of 5digits numbers={sum1}");
+				//sum1 = sum1 + sum;
+				//Console.WriteLine($"sum of sum and sum1={sum1}");
+				//sub = sum1 - ((y * x) * 10 + 1);
+				//Console.WriteLine($"subtraction1={sub}");
+				//sum1 = sub + p;
+				//Console.WriteLine($"sum of subtraction and 6digits numbers={sum1}");
+				//sub = sum1 - (y + x + z + s);
+				//Console.WriteLine($"subtraction2={sub}");
+				//per1 = (sub / 100) * 18;
+				//Console.WriteLine($"18% of subtraction={per1}");
+				//per1 = (per1 / 100) * 3;
+				//Console.WriteLine($"3% of per1={per1}");
+				//per1 = (per1 / 100) * 1;
+				//Console.WriteLine($"1% of per1={per1}");
+				//sum1 = per1 + (n + m);
+				//Console.WriteLine($"sum={sum1}");
+				#endregion
+			}
+
+
+
+
+
+
+
+			static int readint()
+			{
+
+				int a;
+			label1:
+				Console.Write("a:");
+				string b = Console.ReadLine();
+
+				if (int.TryParse(b, out a) == false)
+				{
+					Console.WriteLine("eded daxil etmelisiniz!");
+					goto label1;
+
+				}
+				else
+				{
+					return a;
+				}
+			}
+
+			static int number3()
+			{
+			Label:
+				int y = readint();
+
+
+				if (y > 100 && y < 1000)
+				{
+
+					return y;
+				}
+				else
+				{
+					Console.WriteLine("eded [100,999] araliginda olmalidir!");
+					goto Label;
+				}
+			}
+
+
+
+			static int number4()
+			{
+			Label:
+				int y = readint();
+
+
+				if (y > 1000 && y < 10000)
+				{
+
+					return y;
+				}
+				else
+				{
+					Console.WriteLine("eded [1000,9999] araliginda olmalidir!");
+					goto Label;
+				}
+			}
+
+			static int number6()
+			{
+			Label:
+				int y = readint();
+
+
+				if (y > 100000 && y < 1000000)
+				{
+
+					return y;
+				}
+				else
+				{
+					Console.WriteLine("eded [100000,999999] araliginda olmalidir!");
+					goto Label;
+				}
+			}
+
+			static int number5()
+			{
+			Label:
+				int y = readint();
+
+
+				if (y > 10000 && y < 100000)
+				{
+
+					return y;
+				}
+				else
+				{
+					Console.WriteLine("eded [10000,99999] araliginda olmalidir!");
+					goto Label;
+				}
+			}
+
+			static int number7()
+			{
+			Label:
+				int y = readint();
+
+
+				if (y > 1000000 && y < 10000000)
+				{
+
+					return y;
+				}
+				else
+				{
+					Console.WriteLine("eded [1000000,9999999] araliginda olmalidir!");
+					goto Label;
+				}
+        }
+    }
+}
